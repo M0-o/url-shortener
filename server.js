74a4dbfaec15e5766e7a3dd30c8d230cb6df4280
Mouad Berrami <berrami.mouad2@gmail.com>
2025-01-03 11:12:56 +0100

    improved styling

--- server.js
+++ server.js
@@ -1,61 +1,48 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const dbFunctions = require('./data.js');
+const express = require("express");
+const mongoose = require("mongoose");
+const dbFunctions = require("./data.js");
 
-process.env.MONGO_URI = "mongodb://localhost/url-shortener" ;
+mongoose
+  .connect("mongodb://localhost/url-shortener")
+  .then(() => console.log("database connected succesfully"))
+  .catch((err) => console.log(err));
 
-mongoose.connect(process.env.MONGO_URI)
-.then(()=>console.log("database connected succesfully"))
-.catch((err)=>console.log(err));
+const app = express();
 
-const app = express() ;
-
-
-app.use('/' , express.static(__dirname + "/views"));
+app.use("/", express.static(__dirname + "/views"));
 app.use(express.json());
 
+app.listen(3000, console.log("app listening on port 3000"));
 
-app.listen(3000 , console.log("app listening on port 3000"));
-
-
-app.get('/' , (req ,res)=>{
-    res.sendFile(__dirname + "/views/index.html");
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
 });
 
+app.get("/access/:id", async (req, res) => {
+  const id = req.params.id;
 
+  console.log(id);
 
-app.get('/access/:id' , async (req , res)=>{
-     const id = req.params.id ;
+  const data = await dbFunctions.urlModel.findById(id);
 
-     console.log(id);
+  console.log(data);
 
-      const data = await dbFunctions.urlModel.findById(id)
-
-      console.log(data);
-      
-      res.redirect(data.originalUrl);
+  res.redirect(data.originalUrl);
 });
 
+app.post("/add", async (req, res) => {
+  const url = await dbFunctions.addUrl(req.body.urlString);
 
-app.post('/add' , async (req,res)=>{
+  res.json(url);
+});
 
-    const url =  await dbFunctions.addUrl(req.body.urlString) ;
-   
-    res.json(url);
-})
+app.delete("/remove/:id", async (req, res) => {
+  const deleted = await dbFunctions.urlModel.findByIdAndDelete(req.params.id);
 
-app.delete('/remove/:id' , async (req,res)=>{
+  res.json(deleted);
+});
 
-    const deleted = await dbFunctions.urlModel.findByIdAndDelete(req.params.id);
-
-
-    res.json(deleted);
-
-
-})
-
-app.get('/findAll', async(req,res)=>{
-
-    const allUrls = await  dbFunctions.urlModel.find();
-    res.send(allUrls);
-})
+app.get("/findAll", async (req, res) => {
+  const allUrls = await dbFunctions.urlModel.find();
+  res.send(allUrls);
+});
--- views/index.js
+++ views/index.js
@@ -1,64 +1,53 @@
 const input = document.querySelector("#urlString");
-const container = document.querySelector('#url-cards-container');
+const container = document.querySelector("#url-cards-container");
 
-const shorten = async function(){
+const shorten = async function () {
+  if (!input.value) {
+    alert("input is empty");
+    return;
+  }
 
-    if(!input.value){
-         alert("input is empty");
-        return 
-    }
+  await fetch(`/add`, {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify({
+      urlString: input.value,
+    }),
+  });
+  location.reload();
+};
 
-    await fetch(`/add` , {
-        method: "POST",
-        headers: {
-            "Content-type" : "application/json"
-        },
-        body:JSON.stringify({
-            urlString: input.value 
-        })
-            
-    })
-    
+const getAll = async function () {
+  let urls = await fetch("/findAll", {
+    method: "GET",
+  });
+  urls = await urls.json();
+
+  const arr = [...urls];
+
+  let newArr = arr.map(
+    (data) => `
+      <tr class="url-card">
+        <td>${data.originalUrl}</td>
+        <td><a href="/access/${data._id}" target="blank">http://localhost:3000/access/${data._id}</a></td>
+        <td><button onclick="deleteU('${data._id}')">X</button></td>
+      </tr>
+    `
+  );
+
+  for (let i of newArr) {
+    container.innerHTML += i;
+  }
+};
+
+async function deleteU(id) {
+  await fetch(`/remove/${id}`, {
+    method: "DELETE",
+  });
+
+  location.reload();
 }
 
-
-
-const getAll = async function(){
-
-   let urls = await fetch('/findAll', {
-        method: 'GET'
-    });
-    urls = await urls.json();
-
-    const arr = [...urls];
-    console.log(negro);
-
-    let newArr =  arr.map((data) => `<div class="url-card" >
-    <span>original url: ${data.originalUrl}</span>
-    <span>shortened url: http://localhost:3000/access/${data._id} </span>
-    <button onclick="deleteNegro('${data._id}')">X</button>
-    </div>`
-)
-
- console.log(newArr);
-
-    
-    for(let i of newArr){
-        container.innerHTML+=i;
-    }
-
-}
-
-async function deleteNegro(id){
-
-    await fetch(`/remove/${id}` , {
-        method: 'DELETE',
-    } )
-
-    location.reload();
-
-}
-
-
-  getAll();
-
+getAll();
